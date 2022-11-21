c2847572dc5423413b083638fd1a9a5ff1cc33f7
shingo1013 <117624040+shingo1013@users.noreply.github.com>
2022-11-21 14:46:55 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 $(function () {
 
+  $('.hamburger').on('click', function(){
+    $(this).toggleClass('open');
+    $('#navi').toggleClass('open');
+  });
 
-
-
+  $('#navi a').on('click', function(){
+    $('.hamburger').toggleClass('open');
+    $('#navi').toggleClass('open');
+  });
 
 
 
