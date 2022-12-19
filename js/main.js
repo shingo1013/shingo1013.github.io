ce83703633550506b1668a7273725d8d3c7fd5d0
shingo1013 <117624040+shingo1013@users.noreply.github.com>
2022-12-19 21:53:24 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(function () {
 
   $('a[href^="#"]').click(function () {
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    var position = target.offset().top;
-    var speed = 500;
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    let speed = 500;
     $("html, body").animate({
-      scrollTop: position
+      scrollTop: position - 90
     }, speed, "swing");
     return false;
   });
@@ -22,48 +22,7 @@ $(function () {
   });
 
   $(window).on('scroll', function () {
-    let scroll = $(window).scrollTop();
-    let profile_pos = $('#profile').offset().top - $(window).height() / 2;
-    let career_pos = $('#career').offset().top - $(window).height() / 2;
-    let skill_pos = $('#skill').offset().top - $(window).height() / 2;
-    let works_pos = $('#works').offset().top - $(window).height() / 2;
-    let contact_pos = $('#contact').offset().top - $(window).height();
-    if (scroll == 0) {
-      $('a[href="#profile"]').removeClass('active');
-      $('a[href="#"]').addClass('active');
-    }
-    if (scroll >= profile_pos) {
-      if (scroll < career_pos) {
-        $('a[href="#"]').removeClass('active');
-        $('a[href="#career"]').removeClass('active');
-        $('a[href="#profile"]').addClass('active');
-      }
-    }
-    if (scroll >= career_pos) {
-      if (scroll < skill_pos) {
-        $('a[href="#profile"]').removeClass('active');
-        $('a[href="#skill"]').removeClass('active');
-        $('a[href="#career"]').addClass('active');
-      }
-    }
-    if (scroll >= skill_pos) {
-      if (scroll < works_pos) {
-        $('a[href="#career"]').removeClass('active');
-        $('a[href="#works"]').removeClass('active');
-        $('a[href="#skill"]').addClass('active');
-      }
-    }
-    if (scroll >= works_pos) {
-      if (scroll < contact_pos) {
-        $('a[href="#skill"]').removeClass('active');
-        $('a[href="#contact"]').removeClass('active');
-        $('a[href="#works"]').addClass('active');
-      }
-    }
-    if (scroll >= contact_pos) {
-      $('a[href="#works"]').removeClass('active');
-      $('a[href="#contact"]').addClass('active');
-    }
+    header_sync();
   });
 
 });
@@ -71,4 +30,53 @@ $(function () {
 $(window).on('load', function () {
   $('.leftAnime').addClass("slideAnimeLeftRight");
   $('.leftAnimeInner').addClass("slideAnimeRightLeft");
+  header_sync();
 });
+
+function header_sync() {
+  let scroll = $(window).scrollTop();
+  let profile_pos = $('#profile').offset().top - 90;
+  let career_pos = $('#career').offset().top - 90;
+  let skill_pos = $('#skill').offset().top - 90;
+  let works_pos = $('#works').offset().top - 90;
+  let contact_pos = $('#contact').offset().top - 90;
+  if (scroll == 0) {
+    $('a[href="#profile"]').removeClass('active');
+    $('a[href="#"]').addClass('active');
+  }
+  if (scroll >= profile_pos) {
+    if (scroll <= career_pos) {
+      $('a[href="#"]').removeClass('active');
+      $('a[href="#career"]').removeClass('active');
+      $('a[href="#profile"]').addClass('active');
+    }
+  }
+  if (scroll >= career_pos) {
+    if (scroll < skill_pos) {
+      $('a[href="#profile"]').removeClass('active');
+      $('a[href="#skill"]').removeClass('active');
+      $('a[href="#career"]').addClass('active');
+    }
+  }
+  if (scroll >= skill_pos) {
+    if (scroll < works_pos) {
+      $('a[href="#career"]').removeClass('active');
+      $('a[href="#works"]').removeClass('active');
+      $('a[href="#skill"]').addClass('active');
+    }
+  }
+  if (scroll >= works_pos) {
+    if (scroll < contact_pos) {
+      $('a[href="#skill"]').removeClass('active');
+      $('a[href="#contact"]').removeClass('active');
+      $('a[href="#works"]').addClass('active');
+    }
+  }
+  let docHeight = $(document).innerHeight();
+  let windowHeight = $(window).innerHeight();
+  let pageBottom = docHeight - windowHeight;
+  if (pageBottom <= $(window).scrollTop()) {
+    $('a[href="#works"]').removeClass('active');
+    $('a[href="#contact"]').addClass('active');
+  }
+}
